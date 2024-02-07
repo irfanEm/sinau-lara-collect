179cfec86553f5c55feccd93e362af170316fe1c
progamerxml <progamerxml@outlook.com>
2024-02-07 14:37:18 +0700

    paripurna

--- tests/Feature/CollectTest.php
+++ tests/Feature/CollectTest.php
@@ -247,4 +247,374 @@ class CollectTest extends TestCase
 
         self::assertEqualsCanonicalizing([2,4,6,8,10], $hasil->all());
     }
+
+    public function testPartitioning()
+    {
+        $koleksi = collect([
+            "angga" => 99,
+            "budi" => 98, 
+            "ceri" => 88,
+            "dani" => 99
+        ]);
+
+        [$hasil1, $hasil2] = $koleksi->partition(function($item, $key){
+            return $item >98;
+        });
+
+        self::assertEquals(["angga" => 99, "dani" => 99], $hasil1->all());
+        self::assertEquals(["budi" => 98,"ceri" => 88], $hasil2->all());
+    }
+
+    public function testTesting()
+    {
+        $koleksi = collect(["Balqis", "Farah", "Anabila"]);
+        
+        self::assertTrue($koleksi->has([0,1,2]));
+        self::assertTrue($koleksi->hasAny([1,3]));
+        self::assertTrue($koleksi->contains("Balqis"));
+        self::assertTrue($koleksi->contains(function($item, $key){
+            return $item == "Anabila";
+        }));
+    }
+
+    public function testGrouping()
+    {
+        $koleksi = collect([
+            [
+                'nama' => 'Andi',
+                'alamat' => 'Jl. Merdeka No. 123',
+                'umur' => 25,
+                'jenis_kelamin' => 'Laki-laki'
+            ],
+            [
+                'nama' => 'Budi',
+                'alamat' => 'Jl. Pahlawan No. 456',
+                'umur' => 30,
+                'jenis_kelamin' => 'Laki-laki'
+            ],
+            [
+                'nama' => 'Citra',
+                'alamat' => 'Jl. Jendral Sudirman No. 789',
+                'umur' => 22,
+                'jenis_kelamin' => 'Perempuan'
+            ],
+            [
+                'nama' => 'Dewi',
+                'alamat' => 'Jl. Diponegoro No. 101',
+                'umur' => 28,
+                'jenis_kelamin' => 'Perempuan'
+            ],
+            [
+                'nama' => 'Eko',
+                'alamat' => 'Jl. Gatot Subroto No. 555',
+                'umur' => 35,
+                'jenis_kelamin' => 'Laki-laki'
+            ],
+            [
+                'nama' => 'Fani',
+                'alamat' => 'Jl. Imam Bonjol No. 222',
+                'umur' => 19,
+                'jenis_kelamin' => 'Perempuan'
+            ],
+            [
+                'nama' => 'Gita',
+                'alamat' => 'Jl. Sisingamangaraja No. 333',
+                'umur' => 24,
+                'jenis_kelamin' => 'Perempuan'
+            ],
+            [
+                'nama' => 'Hadi',
+                'alamat' => 'Jl. Sudirman No. 777',
+                'umur' => 32,
+                'jenis_kelamin' => 'Laki-laki'
+            ],
+            [
+                'nama' => 'Indra',
+                'alamat' => 'Jl. Hayam Wuruk No. 444',
+                'umur' => 27,
+                'jenis_kelamin' => 'Laki-laki'
+            ],
+            [
+                'nama' => 'Joko',
+                'alamat' => 'Jl. Ahmad Yani No. 666',
+                'umur' => 29,
+                'jenis_kelamin' => 'Laki-laki'
+            ]
+        ]);
+
+        $hasil = $koleksi->groupBy("jenis_kelamin");
+
+        self::assertEquals([
+            "Laki-laki" => collect([
+                [
+                    'nama' => 'Andi',
+                    'alamat' => 'Jl. Merdeka No. 123',
+                    'umur' => 25,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Budi',
+                    'alamat' => 'Jl. Pahlawan No. 456',
+                    'umur' => 30,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Eko',
+                    'alamat' => 'Jl. Gatot Subroto No. 555',
+                    'umur' => 35,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Hadi',
+                    'alamat' => 'Jl. Sudirman No. 777',
+                    'umur' => 32,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Indra',
+                    'alamat' => 'Jl. Hayam Wuruk No. 444',
+                    'umur' => 27,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Joko',
+                    'alamat' => 'Jl. Ahmad Yani No. 666',
+                    'umur' => 29,
+                    'jenis_kelamin' => 'Laki-laki'
+                ]
+            ]),
+            "Perempuan" => collect([
+                [
+                    'nama' => 'Citra',
+                    'alamat' => 'Jl. Jendral Sudirman No. 789',
+                    'umur' => 22,
+                    'jenis_kelamin' => 'Perempuan'
+                ],
+                [
+                    'nama' => 'Dewi',
+                    'alamat' => 'Jl. Diponegoro No. 101',
+                    'umur' => 28,
+                    'jenis_kelamin' => 'Perempuan'
+                ],
+                [
+                    'nama' => 'Fani',
+                    'alamat' => 'Jl. Imam Bonjol No. 222',
+                    'umur' => 19,
+                    'jenis_kelamin' => 'Perempuan'
+                ],
+                [
+                    'nama' => 'Gita',
+                    'alamat' => 'Jl. Sisingamangaraja No. 333',
+                    'umur' => 24,
+                    'jenis_kelamin' => 'Perempuan'
+                ]
+            ])
+        ], $hasil->all());
+
+        self::assertEquals([
+            "Laki-laki" => collect([
+                [
+                    'nama' => 'Andi',
+                    'alamat' => 'Jl. Merdeka No. 123',
+                    'umur' => 25,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Budi',
+                    'alamat' => 'Jl. Pahlawan No. 456',
+                    'umur' => 30,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Eko',
+                    'alamat' => 'Jl. Gatot Subroto No. 555',
+                    'umur' => 35,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Hadi',
+                    'alamat' => 'Jl. Sudirman No. 777',
+                    'umur' => 32,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Indra',
+                    'alamat' => 'Jl. Hayam Wuruk No. 444',
+                    'umur' => 27,
+                    'jenis_kelamin' => 'Laki-laki'
+                ],
+                [
+                    'nama' => 'Joko',
+                    'alamat' => 'Jl. Ahmad Yani No. 666',
+                    'umur' => 29,
+                    'jenis_kelamin' => 'Laki-laki'
+                ]
+            ]),
+            "Perempuan" => collect([
+                [
+                    'nama' => 'Citra',
+                    'alamat' => 'Jl. Jendral Sudirman No. 789',
+                    'umur' => 22,
+                    'jenis_kelamin' => 'Perempuan'
+                ],
+                [
+                    'nama' => 'Dewi',
+                    'alamat' => 'Jl. Diponegoro No. 101',
+                    'umur' => 28,
+                    'jenis_kelamin' => 'Perempuan'
+                ],
+                [
+                    'nama' => 'Fani',
+                    'alamat' => 'Jl. Imam Bonjol No. 222',
+                    'umur' => 19,
+                    'jenis_kelamin' => 'Perempuan'
+                ],
+                [
+                    'nama' => 'Gita',
+                    'alamat' => 'Jl. Sisingamangaraja No. 333',
+                    'umur' => 24,
+                    'jenis_kelamin' => 'Perempuan'
+                ]
+            ])
+        ], $koleksi->groupBy(function($item, $key){
+            return $item['jenis_kelamin'];
+        })->all());
+    }
+
+    public function testSlicing()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        self::assertEqualsCanonicalizing([6,7,8,9], $koleksi->slice(5)->all());
+        self::assertEqualsCanonicalizing([3,4,5], $koleksi->slice(2, 3)->all());
+    }
+
+    public function testTake()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+
+        $hasilTake = $koleksi->take(3);
+        self::assertEqualsCanonicalizing([1,2,3], $hasilTake->all());
+
+        $hasilTakeUntil = $koleksi->takeUntil(function($item, $key){
+            return $item == 6;
+        });
+        self::assertEqualsCanonicalizing([1,2,3,4,5], $hasilTakeUntil->all());
+
+        $hasilTakeWhile = $koleksi->takeWhile(function($value, $key){
+            return $value < 9;
+        });
+        self::assertEqualsCanonicalizing([1,2,3,4,5,6,7,8], $hasilTakeWhile->all());
+    }
+
+    public function testSkip()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+
+        $hasilSkip = $koleksi->skip(3);
+        self::assertEqualsCanonicalizing([4,5,6,7,8,9], $hasilSkip->all());
+
+        $hasilSkipUntil = $koleksi->skipUntil(function($item, $key){
+            return $item == 6;
+        });
+        self::assertEqualsCanonicalizing([6,7,8,9], $hasilSkipUntil->all());
+
+        $hasilSkipWhile = $koleksi->skipWhile(function($value, $key){
+            return $value < 9;
+        });
+        self::assertEqualsCanonicalizing([9], $hasilSkipWhile->all());
+    }
+
+    public function testChunk()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        $hasil = $koleksi->chunk(3);
+
+        self::assertEqualsCanonicalizing([1,2,3], $hasil->all()[0]->all());
+        self::assertEqualsCanonicalizing([4,5,6], $hasil->all()[1]->all());
+        self::assertEqualsCanonicalizing([7,8,9], $hasil->all()[2]->all());
+    }
+
+    public function testRetrieve()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        $hasil = $koleksi->first();
+        self::assertEqualsCanonicalizing(1, $hasil);
+
+        $hasil = $koleksi->first(function($value, $key){
+            return $value > 8;
+        });
+        self::assertEqualsCanonicalizing(9, $hasil);
+    }
+
+    public function testRetrieveLast()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        $hasil = $koleksi->last();
+        self::assertEqualsCanonicalizing(9, $hasil);
+
+        $hasil = $koleksi->last(function($value, $key){
+            return $value < 8;
+        });
+        self::assertEqualsCanonicalizing(7, $hasil);
+    }
+
+    public function testRandom()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        $hasilRand = $koleksi->random();
+        self::assertTrue(in_array($hasilRand, [1,2,3,4,5,6,7,8,9]));
+    }
+
+    public function testCheckingExtension()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        self::assertTrue($koleksi->isNotEmpty());
+        self::assertFalse($koleksi->isEmpty());
+        self::assertTrue($koleksi->contains(1));
+        self::assertTrue($koleksi->contains(function($value, $key){
+            return $value == 4;
+        }));
+        self::assertFalse($koleksi->containsOneItem());
+    }
+
+    public function testOrdering()
+    {
+        $koleksi = collect([1,4,5,6,2,3,7,9,8]);
+        $hasilAsc = $koleksi->sort();
+
+        self::assertEqualsCanonicalizing([1,2,3,4,5,6,7,8,9], $hasilAsc->all());
+
+        $hasilDesc = $koleksi->sortDesc();
+        self::assertEqualsCanonicalizing([9,8,7,6,5,4,3,2,1], $hasilDesc->all());
+    }
+
+    public function testAgregate()
+    {
+        $koleksi = collect([1,2,3,4,5,6,7,8,9]);
+        
+        $hasil = $koleksi->min();
+        self::assertEquals(1, $hasil);
+
+        $hasilMax = $koleksi->max();
+        self::assertEquals(9, $hasilMax);
+
+        $hasilCount = $koleksi->count();
+        self::assertEquals(9, $hasilCount);
+
+        $hasilSum = $koleksi -> sum();
+        self::assertEquals(45, $hasilSum);
+        
+        $hasilAvg = $koleksi->avg();
+        self::assertEquals(5, $hasilAvg);
+    }
+
+    public function testReduce()
+    {
+        $koleksi = collect([1,3,5,7,9,11,13,15,17,19]);
+        $hasil = $koleksi->reduce(function($carry, $item){
+            return $carry + $item;
+        });
+
+        self::assertEquals(100, $hasil);
+    }
 }
